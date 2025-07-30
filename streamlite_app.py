45425158a4b549df89778b7ba8f3cdf6ab661e62
omnino99 <rinodwilaksono@gmail.com>
2025-07-30 09:15:21 +0800

    Create streamlite_app.py

---
+++ streamlite_app.py
@@ -0,0 +1,5 @@
+project-manager-dashboard/
+├── app.py               ← file utama dashboard
+├── project_data.json    ← penyimpanan data proyek
+├── requirements.txt     ← dependensi Python
+└── README.md            ← penjelasan proyek
